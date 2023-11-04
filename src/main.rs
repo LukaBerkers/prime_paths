435b303c7ea24af1ed2c03a500ca046ce813ff83
Luka <luukberkers@protonmail.com>
2023-11-04 10:13:08 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs};
+
+use eyre::{eyre, Result};
+use graphviz_rust::{
+    dot_structures::Graph,
+    printer::{DotPrinter, PrinterContext},
+};
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    match &args[..] {
+        [_, file_name] => {
+            let contents = fs::read_to_string(file_name)?;
+            let graph = graphviz_rust::parse(&contents).map_err(|err| eyre!(err))?;
+
+            let prime_paths = find_prime_paths(&graph);
+
+            for path in prime_paths {
+                let dot = path.print(&mut PrinterContext::default());
+                println!("{}", dot);
+            }
+
+            Ok(())
+        }
+        _ => Err(eyre!("Wrong number of arguments")),
+    }
+}
+
+fn find_prime_paths(graph: &Graph) -> Vec<Graph> {
+    todo!()
 }
