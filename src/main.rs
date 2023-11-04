3eee938890a97785974ea5f573bd64856f722d42
Luka <luukberkers@protonmail.com>
2023-11-04 15:05:01 +0100

    Reconstruct graph

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, fs};
 
 use eyre::{eyre, Result};
 use graphviz_rust::{
-    dot_structures::{Attribute, Edge, Graph as DotGraph, GraphAttributes, Node, Stmt},
+    dot_structures::{Attribute, Edge, Graph as DotGraph, GraphAttributes, Id, Node, Stmt},
     printer::{DotPrinter, PrinterContext},
 };
 
@@ -14,7 +14,7 @@ fn main() -> Result<()> {
             let contents = fs::read_to_string(file_name)?;
             let graph = graphviz_rust::parse(&contents).map_err(|err| eyre!(err))?;
 
-            let prime_paths = find_prime_paths(&graph);
+            let prime_paths = find_prime_paths(graph);
 
             for path in prime_paths {
                 let dot = path.print(&mut PrinterContext::default());
@@ -43,17 +43,17 @@ struct Graph {
     edges: Vec<Edge>,
 }
 
-fn find_prime_paths(graph: &DotGraph) -> Vec<DotGraph> {
+fn find_prime_paths(graph: DotGraph) -> Vec<DotGraph> {
     let graph = deconstruct_graph(graph);
-    dbg!(graph);
+    let graph = reconstruct_graph(graph, "g");
 
     todo!()
 }
 
-fn deconstruct_graph(graph: &DotGraph) -> Graph {
+fn deconstruct_graph(graph: DotGraph) -> Graph {
     let (graph_type, strict, stmts) = match graph {
-        DotGraph::Graph { strict, stmts, .. } => (GraphType::Undirected, *strict, stmts),
-        DotGraph::DiGraph { strict, stmts, .. } => (GraphType::Directed, *strict, stmts),
+        DotGraph::Graph { strict, stmts, .. } => (GraphType::Undirected, strict, stmts),
+        DotGraph::DiGraph { strict, stmts, .. } => (GraphType::Directed, strict, stmts),
     };
 
     let mut attributes: Vec<Attribute> = Vec::new();
@@ -62,10 +62,10 @@ fn deconstruct_graph(graph: &DotGraph) -> Graph {
     let mut edges: Vec<Edge> = Vec::new();
     for stmt in stmts {
         match stmt {
-            Stmt::Attribute(a) => attributes.push(a.clone()),
-            Stmt::GAttribute(gas) => graph_attributes.push(gas.clone()),
-            Stmt::Node(n) => nodes.push(n.clone()),
-            Stmt::Edge(e) => edges.push(e.clone()),
+            Stmt::Attribute(a) => attributes.push(a),
+            Stmt::GAttribute(gas) => graph_attributes.push(gas),
+            Stmt::Node(n) => nodes.push(n),
+            Stmt::Edge(e) => edges.push(e),
             Stmt::Subgraph(_) => todo!(),
         }
     }
@@ -77,5 +77,29 @@ fn deconstruct_graph(graph: &DotGraph) -> Graph {
         graph_attributes,
         nodes,
         edges,
+    }
+}
+
+fn reconstruct_graph(graph: Graph, id: &str) -> DotGraph {
+    let stmt_iter = graph
+        .attributes
+        .into_iter()
+        .map(Stmt::Attribute)
+        .chain(graph.graph_attributes.into_iter().map(Stmt::GAttribute))
+        .chain(graph.nodes.into_iter().map(Stmt::Node))
+        .chain(graph.edges.into_iter().map(Stmt::Edge));
+    let stmts = Vec::from_iter(stmt_iter);
+
+    match graph.graph_type {
+        GraphType::Undirected => DotGraph::Graph {
+            id: Id::Plain(id.to_owned()),
+            strict: graph.strict,
+            stmts,
+        },
+        GraphType::Directed => DotGraph::DiGraph {
+            id: Id::Plain(id.to_owned()),
+            strict: graph.strict,
+            stmts,
+        },
     }
 }
